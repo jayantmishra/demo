d907ff60d903a159c21e9d3edcec748d6af02105
jayantmishra <jayant.neymar@gmail.com>
2021-12-19 16:22:36 +0530

    Initial commit

---
+++ src/test/java/com/udaan/lld/covid/demo/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udaan.lld.covid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
